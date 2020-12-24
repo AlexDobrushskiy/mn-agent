d6f5e1f2f5ef29973aebad7dfba73cfdcfdc79ea
samodurov_alexander <samodurov_alexander@mail.ru>
2020-12-24 12:46:41 +0300

    MNE-3: refactor Chunk view MNE-2: refactor Regticket view

--- server/main/views.py
+++ server/main/views.py
@@ -37,78 +37,20 @@ class MasternodeApiView(generics.UpdateAPIView):
         obj, created = Masternode.objects.get_or_create(ip=ip)
         return obj
 
-class RegticketApiView(generics.UpdateAPIView):
+
+class RegticketApiView(generics.CreateAPIView):
     serializer_class = RegticketSerializer
-    http_method_names = ['post']
-    def post(self, p):
-        artist_pastelid = self.request.data.get('artist_pastelid')
-        image_hash = self.request.data.get('image_hash')
-        pastelID = self.request.data.get('masternode_pastelid')
-        if not artist_pastelid:
-            raise ValidationError({'artist_pastelid': ["This field is required."]})
 
-        if not image_hash:
-            raise ValidationError({'image_hash': ["This field is required."]})
-        if not Regticket.objects.filter(image_hash=image_hash).count() == 0:
-            raise ValidationError({'image_hash': ["Must be unique"]})
+    def perform_create(self, serializer_class):
+        serializer_class.save()
 
-        if not pastelID:
-            raise ValidationError({'masternode_pastelid': ["This field is required."]})
-        try:
-            masternode = Masternode.objects.get(pastelID=pastelID)
-        except ObjectDoesNotExist:
-            raise ValidationError({'masternode_pastelid': ["DoesNotExist"]})
 
-        obj, created = Regticket.objects.get_or_create(artist_pastelid=artist_pastelid,
-                                                       image_hash=image_hash,
-                                                       masternode_pastelid=masternode,
-                                                       defaults={'status': '0',
-                                                                 'created': datetime.now(),})
-        serializer = RegticketSerializer(obj)
-        return Response(serializer.data)
-
-class ChunkApiView(generics.UpdateAPIView):
+class ChunkApiView(generics.CreateAPIView):
     serializer_class = ChunkSerializer
-    http_method_names = ['post']
-    def post(self, p):
-        fields = ("mn_pastelid", "chunk_id", "image_hash", "indexed", "confirmed", "stored")
-        data = {}
-        for field in fields:
-            data[field] = self.request.data.get(field)
-            if not data[field]:
-                raise ValidationError({field: ["This field is required."]})
 
-        mn_pastelid = data['mn_pastelid']
-        chunk_id = data['chunk_id']
-        image_hash = data['image_hash']
-        indexed = data['indexed']
-        confirmed = data['confirmed']
-        stored = data['stored']
+    def perform_create(self, serializer_class):
+        serializer_class.save()
 
-
-        if indexed != "True" and indexed != "False":
-            raise ValidationError({'indexed': ["This field must be 'True' or 'False'"]})
-        if confirmed != "True" and confirmed != "False":
-            raise ValidationError({'confirmed': ["This field must be 'True' or 'False'"]})
-        if stored != "True" and stored != "False":
-            raise ValidationError({'stored': ["This field must be 'True' or 'False'"]})
-
-
-        if not Chunk.objects.filter(chunk_id=chunk_id).count() == 0:
-            raise ValidationError({'chunk_id': ["Must be unique"]})
-        try:
-            pastelID = Masternode.objects.get(pastelID=mn_pastelid)
-        except ObjectDoesNotExist:
-            raise ValidationError({'masternode_pastelid': ["DoesNotExist"]})
-
-        obj, created = Chunk.objects.get_or_create(mn_pastelid=pastelID,
-                                                   chunk_id=chunk_id,
-                                                   image_hash=image_hash,
-                                                   indexed=indexed,
-                                                   confirmed=confirmed,
-                                                   stored=stored,)
-        serializer = ChunkSerializer(obj)
-        return Response(serializer.data)
 
 def show_masternode_data(request):
     field_names = ['id', 'ip', 'address', 'balance', 'pastelID']
